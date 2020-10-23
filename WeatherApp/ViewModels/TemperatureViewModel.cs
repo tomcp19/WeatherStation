8fe8ab3480e4e08ba245bfc7348f1c762206d88a
Nick B <nbourrereg@hotmail.com>
2020-10-23 14:49:07 -0400

    Comment with secret code

--- WeatherApp/ViewModels/TemperatureViewModel.cs
+++ WeatherApp/ViewModels/TemperatureViewModel.cs
@@ -6,6 +6,6 @@ namespace WeatherApp.ViewModels
 {
     public class TemperatureViewModel : BaseViewModel
     {
-        /// TODO : Ajoutez le code nécessaire pour réussir les tests et répondre aux requis du projet
+        /// La réponse est ici : http://bit.ly/dellorkcir
     }
 }
